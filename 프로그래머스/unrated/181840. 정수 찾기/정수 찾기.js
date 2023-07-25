fca47574de26e1b51da9cbd7c5f9790610d08bf1
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2023-07-26 03:23:18 +0900

    [unrated] Title: 정수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -5,3 +5,11 @@ function solution(num_list, n) {
     }
     return answer;
 }
+//여부를 생각할 땐 includes로 true/false를 가려내도 좋다
+// function solution(num_list, n) {
+//     var answer = 0;
+//     if(num_list.includes(n) == true){
+//         answer=1
+//     }
+//     return answer;
+// }
