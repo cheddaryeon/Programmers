4db7199a7db188ce4fe876027f2d0ba02d2e9de3
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2023-07-28 10:01:49 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 8.73 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
@@ -2,9 +2,7 @@ function solution(myString) {
     let newlist = []
     let list = myString.split("x").sort()
     list.map((i)=>{
-        if(i !== ""){
-            newlist.push(i)
-        }
+        i ===""? null : newlist.push(i)
     })
     return newlist
 }
