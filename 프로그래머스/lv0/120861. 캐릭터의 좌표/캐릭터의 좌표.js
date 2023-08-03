89ff6169ccbd612a95e498eee00a2d1cfe6f2adb
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2023-08-03 10:08:59 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,18 +1,18 @@
 function solution(keyinput, board) {
     let center=[0,0]
-    let half_x = board[0]/2
-    let half_y = board[1]/2
+    let half_x = board[0]/2-1
+    let half_y = board[1]/2-1
     keyinput.map((i)=>{
-        if(i === "left" && -half_x<center[0]-1){
+        if(i === "left" && -half_x<center[0]){
             center[0]-=1
         }
-        else if(i === "right" && center[0]+1<half_x){
+        else if(i === "right" && center[0]<half_x){
             center[0] += 1
         }
-        else if(i === "up" && center[1]+1<half_y){
+        else if(i === "up" && center[1]<half_y){
             center[1] += 1
         }
-        else if(i === "down" && -half_y<center[1]-1){
+        else if(i === "down" && -half_y<center[1]){
             center[1] -= 1
         }
     })
