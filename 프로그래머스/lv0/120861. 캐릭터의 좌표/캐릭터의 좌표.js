8c9a3658a0237863a3e0ee60de0da4f5d2a4c2e4
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2023-08-03 09:53:54 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,29 +1,18 @@
-// function solution(keyinput, board) {
-//     let center=[0,0]
-//     keyinput.map((i)=>{
-//         i === "left"? center[0]-=1 : i === "right"? center[0] += 1 : i==="up" ? center[1] += 1 : center[1] -=1
-//     })
-//     if(center[0] < -(board[0]/2) || (board[0]/2) < center[0]){
-//         center[0]>0 ? center[0] = Math.trunc(board[0]/2) : center[0] =  -Math.trunc(board[0]/2)
-//     }
-//     if(center[1] < -(board[1]/2) || (board[1]/2) < center[1]){
-//         center[1]>0 ? center[1] =  Math.trunc(board[1]/2) : center[1] =  -Math.trunc(board[1]/2)
-//     }
-//     return center
-// }
 function solution(keyinput, board) {
     let center=[0,0]
+    let half_x = board[0]/2
+    let half_y = board[1]/2
     keyinput.map((i)=>{
-        if(i === "left" && -(board[0]/2)<center[0]-1 && center[0]-1<(board[0]/2)){
+        if(i === "left" && -half_x<center[0]-1 && center[0]-1<half_x){
             center[0]-=1
         }
-        else if(i === "right" && -(board[0]/2)<center[0]+1 && center[0]+1<(board[0]/2)){
+        else if(i === "right" && -half_x<center[0]+1 && center[0]+1<half_x){
             center[0] += 1
         }
-        else if(i === "up" && -(board[1]/2)<center[1]+1 && center[1]+1<(board[1]/2)){
+        else if(i === "up" && -half_y<center[1]+1 && center[1]+1<half_y){
             center[1] += 1
         }
-        else if(i === "down" && -(board[1]/2)<center[1]-1 && center[1]-1<(board[1]/2)){
+        else if(i === "down" && -half_y<center[1]-1 && center[1]-1<half_y){
             center[1] -= 1
         }
     })
