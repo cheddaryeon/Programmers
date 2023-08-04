63f995bafd2d784fb005b047c89a265f51909b4b
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2023-08-04 10:28:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,8 +1,9 @@
 function solution(n) {
     let name = ""
-    for(i=0;i<=n/2;i++){
+    for(i=0;i<n/2;i++){
         name += "수박"
     }
-    return n%2? name.slice(0,-1) : name.slice(0,-2);
+    return n%2? name.slice(0,-1) : name;
 }
-//"a=2a-a"을 생각했다면 더 간결하게 쓸 수 있을 듯
+//"n=2n-n"을 생각했다면 더 간결하게 쓸 수 있을 듯 +repeat
+//2는 '수박' 글자 수
