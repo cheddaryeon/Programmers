37e36e90cb9625aab23a7b5e3f979060e7478484
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2023-08-06 22:04:24 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = [];
+    let x = num_list.length-1
+    for(i=0; i<x/2; i++){
+        answer = num_list[x-i]
+        num_list[x-i] = num_list[i]
+        num_list[i] = answer
+    }
+    return num_list;
+}
