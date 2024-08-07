43a6d5bcb193f8e6863e4a3bf210ddbdf20a2973
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2024-08-07 16:53:59 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,20 +1,21 @@
-function solution(my_string, alp){
-    var answer = '';
-    for (let i=0; i<my_string.length; i++){
-        if(my_string[i] == alp){
-            answer += alp.toUpperCase()
-        }
-        else{
-            answer += my_string[i]
-        }
-    }
-    return answer;
-}
-
-// function solution(my_string, alp) {
+// function solution(my_string, alp){
 //     var answer = '';
-//     answer = my_string.replace(alp,alp.toUpperCase())
+//     for (let i=0; i<my_string.length; i++){
+//         if(my_string[i] == alp){
+//             answer += alp.toUpperCase()
+//         }
+//         else{
+//             answer += my_string[i]
+//         }
+//     }
 //     return answer;
 // }
-//replace는 원본 문자열을 변경하지 않는다. 첫번째 발견 문자만 수행.
+
+function solution(my_string, alp) {
+    var answer = '';
+    answer = my_string.replaceAll(alp,alp.toUpperCase())
+    return answer;
+}
+//replace는 원본 문자열을 변경하지 않는다. 첫번째 발견 문자만 수행
+//replaceAll을 사용하면 모든 문자 변경가능
 //toUpperCase(), toLowerCase()
