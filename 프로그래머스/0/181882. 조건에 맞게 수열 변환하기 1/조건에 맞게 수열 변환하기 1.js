cdf6e4f4bf35f41c420b0761c14bd8760f022e7c
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2024-08-07 16:22:09 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 73.94 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,15 +1,26 @@
 function solution(arr) {
-    var answer = [];
-    for (let i=0; i<arr.length; i++){
-        if(arr[i] >= 50 & arr[i]%2 == 0){
-            answer.push(arr[i]/2)
+    // var answer = [];
+    // for (let i=0; i<arr.length; i++){
+    //     if(arr[i] >= 50 & arr[i]%2 == 0){
+    //         answer.push(arr[i]/2)
+    //     }
+    //     else if(arr[i] < 50 & arr[i]%2 == 1){
+    //         answer.push(arr[i]*2)
+    //     }
+    //     else{
+    //         answer.push(arr[i])
+    //     }
+    // }
+    let answer = arr.map(function(element){
+        if(element >= 50 && element%2 == 0){
+            return element/2
         }
-        else if(arr[i] < 50 & arr[i]%2 == 1){
-            answer.push(arr[i]*2)
+        else if (element < 50 && element%2 == 1){
+            return element*2
         }
         else{
-            answer.push(arr[i])
+            return element
         }
-    }
+    });
     return answer;
 }
