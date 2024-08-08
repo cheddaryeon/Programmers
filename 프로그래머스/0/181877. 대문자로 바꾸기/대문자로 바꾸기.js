87589bad57aae3307952e621f25f9b9acda60f70
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2024-08-08 10:56:05 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,12 +1,15 @@
-function solution(myString) {
-    var answer = '';
-    for (let i=0; i<myString.length; i++){
-        if(myString[i]>='a' && myString<='z'){
-            answer += myString[i].toUpperCase()
-        }
-        else{
-            answer += myString[i]
-        }
-    }
-    return answer;
+// function solution(myString) {
+//     var answer = '';
+//     for (let i=0; i<myString.length; i++){
+//         if(myString[i]>='a' && myString<='z'){
+//             answer += myString[i].toUpperCase()
+//         }
+//         else{
+//             answer += myString[i]
+//         }
+//     }
+//     return answer;
+// }
+function solution(myString){
+    return myString.toUpperCase()
 }
