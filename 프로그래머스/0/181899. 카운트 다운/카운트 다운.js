d2d67fee7ff9b3983efbdddd667d3393db52e304
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2024-08-08 17:28:58 +0900

    [level 0] Title: 카운트 다운, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,7 +1,14 @@
-function solution(start_num, end_num) {
+// function solution(start_num, end_num) {
+//     var answer = [];
+//     for (let i=0; i<=start_num-end_num; i++){
+//         answer.push(start_num-i)
+//     }
+//     return answer;
+// }
+function solution(start_num, end_num){
     var answer = [];
-    for (let i=0; i<=start_num-end_num; i++){
-        answer.push(start_num-i)
+    for (let i=start_num; i>=end_num; i--){
+        answer.push(i)
     }
     return answer;
 }
