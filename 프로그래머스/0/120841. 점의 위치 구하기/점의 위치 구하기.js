469198d7fca0949de671c9fb1d0c9e6510b92fc4
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2024-08-21 22:37:55 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,18 @@
+function solution(dot) {
+    if(dot[0] < 0){
+        if(dot[1]<0){
+            return 3;
+        }
+        else{
+            return 2;
+        }
+    }
+    else{
+        if(dot[1]<0){
+            return 4;
+        }
+        else{
+            return 1;
+        }
+    }
+}
