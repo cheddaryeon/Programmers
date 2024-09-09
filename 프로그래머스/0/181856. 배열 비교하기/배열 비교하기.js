7bcb2861d7e41834d1b7f1afa3c0f3d004032171
cheddaryeon <132885243+cheddaryeon@users.noreply.github.com>
2024-09-09 18:45:36 +0900

    [level 0] Title: 배열 비교하기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,11 +1,5 @@
 function solution(arr1, arr2) {
-    if(arr1.length < arr2.length){
-        return -1;
-    }
-    else if(arr1.length > arr2.length){
-        return 1;
-    }
-    else{
+    if(arr1.length === arr2.length){
         if(arr1.reduce((a,b) => a+b,0) < arr2.reduce((a,b) => a+b,0)){
             return -1;
         }
@@ -15,6 +9,8 @@ function solution(arr1, arr2) {
         else{
             return 0;
         }
-        
+    }
+    else{
+        return arr1.length < arr2.length ? -1: 1;
     }
 }
